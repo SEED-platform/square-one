32296484eec4e7a7c2fb784fcb968c2eb2362214
Nicholas Long <nicholas.long@nrel.gov>
2025-07-04 22:14:51 -0600

    fix search for map-based loading

--- angular-app/src/app/map-workflow/map-workflow.component.ts
+++ angular-app/src/app/map-workflow/map-workflow.component.ts
@@ -17,41 +17,67 @@ export class MapWorkflowComponent implements AfterViewInit {
   private draw!: MapboxDraw;
 
   ngAfterViewInit(): void {
-    this.map = new mapboxgl.Map({
-      accessToken: environment.mapboxToken,
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-77.0369, 38.9072],
-      zoom: 15
-    });
+    console.log('MapWorkflowComponent: ngAfterViewInit called');
+    console.log('Mapbox token:', environment.mapboxToken);
 
-    this.map.addControl(new mapboxgl.NavigationControl());
+    try {
+      this.map = new mapboxgl.Map({
+        accessToken: environment.mapboxToken,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-77.0369, 38.9072],
+        zoom: 15
+      });
 
-    const geocoder = new MapboxGeocoder({
-      accessToken: environment.mapboxToken,
-      mapboxgl: mapboxgl,
-      marker: true,
-      placeholder: 'Search for a location',
-      proximity: { longitude: -77.0369, latitude: 38.9072 },
-      countries: 'us',
-    });
+      console.log('Map created successfully');
 
-    this.map.addControl(geocoder);
+      this.map.addControl(new mapboxgl.NavigationControl());
 
-    this.draw = new MapboxDraw({
-      displayControlsDefault: false,
-      controls: {
-        polygon: true,
-        trash: true
-      }
-    });
+      this.map.on('load', () => {
+        console.log('Map loaded, adding geocoder');
 
-    this.map.addControl(this.draw);
+        const geocoder = new MapboxGeocoder({
+          accessToken: environment.mapboxToken,
+          mapboxgl: mapboxgl,
+          marker: true,
+          placeholder: 'Search for a location',
+          proximity: { longitude: -77.0369, latitude: 38.9072 },
+          countries: 'us',
+        });
 
-    // bind various draw methods
-    this.map.on('draw.create', this.exportGeoJSON.bind(this));
-    this.map.on('draw.update', this.exportGeoJSON.bind(this));
-    this.map.on('draw.delete', this.exportGeoJSON.bind(this));
+        console.log('Geocoder created:', geocoder);
+
+        geocoder.on('result', (e: any) => {
+          console.log('Geocoder result:', e);
+        });
+
+        geocoder.on('error', (e: any) => {
+          console.error('Geocoder error:', e);
+        });
+
+        this.map.addControl(geocoder);
+        console.log('Geocoder added to map');
+
+        this.draw = new MapboxDraw({
+          displayControlsDefault: false,
+          controls: {
+            polygon: true,
+            trash: true
+          }
+        });
+
+        this.map.addControl(this.draw);
+        console.log('Draw control added to map');
+
+        // bind various draw methods
+        this.map.on('draw.create', this.exportGeoJSON.bind(this));
+        this.map.on('draw.update', this.exportGeoJSON.bind(this));
+        this.map.on('draw.delete', this.exportGeoJSON.bind(this));
+      });
+
+    } catch (error) {
+      console.error('Error initializing map:', error);
+    }
   }
 
   exportGeoJSON(): void {
