6906b281dba7088238d72dfddc1bddb9b989c6f5
Nicholas Long <nicholas.long@nrel.gov>
2025-07-13 19:22:00 -0600

    theme legacy

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+// Register AG Grid modules
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 // Disable Mapbox telemetry -- hopefully this will prevent any telemetry data from being sent
 (window as any).MapboxGLTelemetryDisabled = true;
